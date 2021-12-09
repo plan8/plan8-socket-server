41bc69e097db83fa10c670c4dc627eb992e48809
torstah <tor@plan8.se>
2021-12-09 16:18:31 +0100

    env port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = require("socket.io")(server, {
     origin: '*',
   }
 });
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
